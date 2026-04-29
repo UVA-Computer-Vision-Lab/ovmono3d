936132270edeafdeb75c813dd9da2faa1e0e56ec
yaojin17 <1220472926@qq.com>
2026-04-29 01:33:46 -0400

    remove SBATCH headers from training script

--- scripts/dino_bs64_unidepth_omni3d.sh
+++ scripts/dino_bs64_unidepth_omni3d.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=bs64_roi_unidepth_omni3d
-#SBATCH --output=slurm/%x_%j.out
-#SBATCH --error=slurm/%x_%j.err
-#SBATCH --partition=dedicated
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=94
-#SBATCH --gres=gpu:8
-#SBATCH --mem=740G
-#SBATCH --time=6-00:00:00
-#SBATCH --mail-type=begin,end,fail
-
-
 python tools/train_net.py --config-file configs/OVMono3D_dinov2_SFP.yaml  --dist-url auto --resume \
  --num-gpus 8    \
  OUTPUT_DIR  output/bs64_roi_unidepth_omni3d    \
